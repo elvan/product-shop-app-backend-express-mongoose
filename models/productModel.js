d0b797cc20d706e5ca1f19a9c10b40113a6fb5f6
Elvan Hidayat <elvanhd@gmail.com>
2023-04-29 18:16:04 +0700

    creating data seeder script

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose';
-import reviewSchema from './reviewSchema';
+
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const productSchema = new mongoose.Schema(
   {
